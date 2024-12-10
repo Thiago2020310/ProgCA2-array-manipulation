39b7644d372029ff5f4830c34b542944a467af7f
Thiago2020310 <2020310@student.cct.ie>
2024-12-10 11:38:47 +0000

    Task 1 completed.
    Using scanner to get user inputs.
    The program asks for the user array size.
    Then asks for the array elements based on the array size.
    A for loop runs through the elements on the array to check if the value is duplicate. The outer loop hold the number to be compared, the inner loop is the comparison.
    A boolean hold the value isDuplicate, if the value is false, it measn that the loop did not found any duplicates and send the correct message to the user.

--- CA2-project/src/ca2/project/CA2Project.java
+++ CA2-project/src/ca2/project/CA2Project.java
@@ -4,6 +4,9 @@
  */
 package ca2.project;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author User
@@ -14,7 +17,56 @@ public class CA2Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        try{
+            Scanner input = new Scanner(System.in);
+            /*
+            Task 1: Array Search – Find the First Repeated Element
+            Take the size of the array and its elements as input.
+                - First declare the array.
+                - Get the size of the array from the user
+                - Get the elements of the array from the user, based on the size input
+            Find and print the first element that repeats in the array (i.e., occurs more than once).
+                - Loop through the array:
+                    - Get next index and loop through the array to check if equals.
+                    - If no element repeats, print "No repeated elements found."
+            
+            System.out.println("Enter the Size of the array: ");
+            int arraySize = input.nextInt();
+            System.out.println("Enter the elements of the array: ");
+            int[] arrayElements = new int[arraySize];
+            int tempElement = input.nextInt();
+            arrayElements[0] = tempElement;
+            for(int i = 1; i < arraySize; i++){
+                System.out.println("Enter the next element of the array: ");
+                tempElement = input.nextInt();
+                arrayElements[i] = tempElement;
+            }
+            System.out.println("Your array is " + Arrays.toString(arrayElements));
+            boolean isDuplicate = false;
+            for(int i = 0; i < arrayElements.length; i++){
+                for(int j = i+1; j < arrayElements.length; j++){
+                    if(arrayElements[i] == arrayElements[j]){
+                        System.out.println("Duplicate element found: " + arrayElements[i] + " and " + arrayElements[j]);
+                        isDuplicate = true;
+                        return;
+                    }
+                }
+            }
+            if(!isDuplicate){
+                System.out.println("No duplicate elements found.");
+            }
+            
+            */
+            
+        }catch(Exception e){
+            System.out.println(e.getLocalizedMessage());
+        }
+
+        
+        
+        
+
     }
     
 }
